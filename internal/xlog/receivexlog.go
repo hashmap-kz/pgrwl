0a22332071abdda46a037dc440a875f43b43bdd5
alexey.zh <mailbox.stdio@gmail.com>
2025-04-26 15:06:55 +0500

    fix

--- internal/xlog/receivexlog.go
+++ internal/xlog/receivexlog.go
@@ -2,6 +2,7 @@ package xlog
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -31,127 +32,18 @@ type walfileT struct {
 var walfile *walfileT
 
 type StreamCtl struct {
-	StartPos               pglogrepl.LSN
-	Timeline               uint32
-	StandbyMessageTimeout  time.Duration
-	Synchronous            bool
-	PartialSuffix          string
-	StreamStop             func(blockpos pglogrepl.LSN, timeline uint32, endOfSegment bool) bool
-	SendFeedback           func(conn *pgconn.PgConn, blockpos pglogrepl.LSN, now time.Time, replyRequested bool) error
-	FlushWAL               func() error
-	WriteXLogData          func(xld *pglogrepl.XLogData) error
-	WriteKeepaliveResponse func() error
+	StartPos              pglogrepl.LSN
+	Timeline              uint32
+	StandbyMessageTimeout time.Duration
+	Synchronous           bool
+	PartialSuffix         string
+	StreamStop            func(blockpos pglogrepl.LSN, timeline uint32, endOfSegment bool) bool
 }
 
 func (w *walfileT) Sync() error {
 	// TODO:fix
 	return nil
 }
-
-/*
-//  * Open a new WAL file in the specified directory.
-//  *
-//  * Returns true if OK; on failure, returns false after printing an error msg.
-//  * On success, 'walfile' is set to the opened WAL file.
-//  *
-//  * The file will be padded to 16Mb with zeroes.
-//  */
-// static bool
-// open_walfile(StreamCtl *stream, XLogRecPtr startpoint)
-// {
-// 	Walfile    *f;
-// 	char	   *fn;
-// 	ssize_t		size;
-// 	XLogSegNo	segno;
-// 	char		walfile_name[MAXPGPATH];
-//
-// 	XLByteToSeg(startpoint, segno, WalSegSz);
-// 	XLogFileName(walfile_name, stream->timeline, segno, WalSegSz);
-//
-// 	/* Note that this considers the compression used if necessary */
-// 	fn = stream->walmethod->ops->get_file_name(stream->walmethod,
-// 											   walfile_name,
-// 											   stream->partial_suffix);
-//
-// 	/*
-// 	 * When streaming to files, if an existing file exists we verify that it's
-// 	 * either empty (just created), or a complete WalSegSz segment (in which
-// 	 * case it has been created and padded). Anything else indicates a corrupt
-// 	 * file. Compressed files have no need for padding, so just ignore this
-// 	 * case.
-// 	 *
-// 	 * When streaming to tar, no file with this name will exist before, so we
-// 	 * never have to verify a size.
-// 	 */
-// 	if (stream->walmethod->compression_algorithm == PG_COMPRESSION_NONE &&
-// 		stream->walmethod->ops->existsfile(stream->walmethod, fn))
-// 	{
-// 		size = stream->walmethod->ops->get_file_size(stream->walmethod, fn);
-// 		if (size < 0)
-// 		{
-// 			pg_log_error("could not get size of write-ahead log file \"%s\": %s",
-// 						 fn, GetLastWalMethodError(stream->walmethod));
-// 			pg_free(fn);
-// 			return false;
-// 		}
-// 		if (size == WalSegSz)
-// 		{
-// 			/* Already padded file. Open it for use */
-// 			f = stream->walmethod->ops->open_for_write(stream->walmethod, walfile_name, stream->partial_suffix, 0);
-// 			if (f == NULL)
-// 			{
-// 				pg_log_error("could not open existing write-ahead log file \"%s\": %s",
-// 							 fn, GetLastWalMethodError(stream->walmethod));
-// 				pg_free(fn);
-// 				return false;
-// 			}
-//
-// 			/* fsync file in case of a previous crash */
-// 			if (stream->walmethod->ops->sync(f) != 0)
-// 			{
-// 				pg_log_error("could not fsync existing write-ahead log file \"%s\": %s",
-// 							 fn, GetLastWalMethodError(stream->walmethod));
-// 				stream->walmethod->ops->close(f, CLOSE_UNLINK);
-// 				exit(1);
-// 			}
-//
-// 			walfile = f;
-// 			pg_free(fn);
-// 			return true;
-// 		}
-// 		if (size != 0)
-// 		{
-// 			/* if write didn't set errno, assume problem is no disk space */
-// 			if (errno == 0)
-// 				errno = ENOSPC;
-// 			pg_log_error(ngettext("write-ahead log file \"%s\" has %zd byte, should be 0 or %d",
-// 								  "write-ahead log file \"%s\" has %zd bytes, should be 0 or %d",
-// 								  size),
-// 						 fn, size, WalSegSz);
-// 			pg_free(fn);
-// 			return false;
-// 		}
-// 		/* File existed and was empty, so fall through and open */
-// 	}
-//
-// 	/* No file existed, so create one */
-//
-// 	f = stream->walmethod->ops->open_for_write(stream->walmethod,
-// 											   walfile_name,
-// 											   stream->partial_suffix,
-// 											   WalSegSz);
-// 	if (f == NULL)
-// 	{
-// 		pg_log_error("could not open write-ahead log file \"%s\": %s",
-// 					 fn, GetLastWalMethodError(stream->walmethod));
-// 		pg_free(fn);
-// 		return false;
-// 	}
-//
-// 	pg_free(fn);
-// 	walfile = f;
-// 	return true;
-// }
 
 func openWalFile(stream *StreamCtl, startpoint uint64) (*os.File, string, error) {
 	segno := XLByteToSeg(startpoint, WalSegSz)
@@ -197,52 +89,6 @@ func openWalFile(stream *StreamCtl, startpoint uint64) (*os.File, string, error)
 	return fd, fullPath, nil
 }
 
-// /*
-//  * Close the current WAL file (if open), and rename it to the correct
-//  * filename if it's complete. On failure, prints an error message to stderr
-//  * and returns false, otherwise returns true.
-//  */
-// static bool
-// close_walfile(StreamCtl *stream, XLogRecPtr pos)
-// {
-// 	strlcpy(walfile_name, walfile->pathname, MAXPGPATH);
-// 	currpos = walfile->currpos;
-//
-// 	/* Note that this considers the compression used if necessary */
-// 	fn = stream->walmethod->ops->get_file_name(stream->walmethod,
-// 											   walfile_name,
-// 											   stream->partial_suffix);
-//
-// 	if (stream->partial_suffix)
-// 	{
-// 		if (currpos == WalSegSz)
-// 			r = stream->walmethod->ops->close(walfile, CLOSE_NORMAL);
-// 		else
-// 		{
-// 			pg_log_info("not renaming \"%s\", segment is not complete", fn);
-// 			r = stream->walmethod->ops->close(walfile, CLOSE_NO_RENAME);
-// 		}
-// 	}
-// 	else
-// 		r = stream->walmethod->ops->close(walfile, CLOSE_NORMAL);
-//
-// 	walfile = NULL;
-//
-// 	if (r != 0)
-// 	{
-// 		pg_log_error("could not close file \"%s\": %s",
-// 					 fn, GetLastWalMethodError(stream->walmethod));
-//
-// 		pg_free(fn);
-// 		return false;
-// 	}
-//
-// 	pg_free(fn);
-//
-// 	lastFlushPosition = pos;
-// 	return true;
-// }
-
 func closeWalfile(stream *StreamCtl, pos uint64) error {
 	if walfile == nil {
 		return nil
@@ -285,137 +131,6 @@ func closeAndRename() error {
 	}
 	return nil
 }
-
-// /*
-//  * Process XLogData message.
-//  */
-// static bool
-// ProcessXLogDataMsg(PGconn *conn, StreamCtl *stream, char *copybuf, int len,
-// 				   XLogRecPtr *blockpos)
-// {
-// 	int			xlogoff;
-// 	int			bytes_left;
-// 	int			bytes_written;
-// 	int			hdr_len;
-//
-// 	/*
-// 	 * Once we've decided we don't want to receive any more, just ignore any
-// 	 * subsequent XLogData messages.
-// 	 */
-// 	if (!(still_sending))
-// 		return true;
-//
-// 	/*
-// 	 * Read the header of the XLogData message, enclosed in the CopyData
-// 	 * message. We only need the WAL location field (dataStart), the rest of
-// 	 * the header is ignored.
-// 	 */
-// 	hdr_len = 1;				/* msgtype 'w' */
-// 	hdr_len += 8;				/* dataStart */
-// 	hdr_len += 8;				/* walEnd */
-// 	hdr_len += 8;				/* sendTime */
-// 	if (len < hdr_len)
-// 	{
-// 		pg_log_error("streaming header too small: %d", len);
-// 		return false;
-// 	}
-// 	*blockpos = fe_recvint64(&copybuf[1]);
-//
-// 	/* Extract WAL location for this block */
-// 	xlogoff = XLogSegmentOffset(*blockpos, WalSegSz);
-//
-// 	/*
-// 	 * Verify that the initial location in the stream matches where we think
-// 	 * we are.
-// 	 */
-// 	if (walfile == NULL)
-// 	{
-// 		/* No file open yet */
-// 		if (xlogoff != 0)
-// 		{
-// 			pg_log_error("received write-ahead log record for offset %u with no file open",
-// 						 xlogoff);
-// 			return false;
-// 		}
-// 	}
-// 	else
-// 	{
-// 		/* More data in existing segment */
-// 		if (walfile->currpos != xlogoff)
-// 		{
-// 			pg_log_error("got WAL data offset %08x, expected %08x",
-// 						 xlogoff, (int) walfile->currpos);
-// 			return false;
-// 		}
-// 	}
-//
-// 	bytes_left = len - hdr_len;
-// 	bytes_written = 0;
-//
-// 	while (bytes_left)
-// 	{
-// 		int			bytes_to_write;
-//
-// 		/*
-// 		 * If crossing a WAL boundary, only write up until we reach wal
-// 		 * segment size.
-// 		 */
-// 		if (xlogoff + bytes_left > WalSegSz)
-// 			bytes_to_write = WalSegSz - xlogoff;
-// 		else
-// 			bytes_to_write = bytes_left;
-//
-// 		if (walfile == NULL)
-// 		{
-// 			if (!open_walfile(stream, *blockpos))
-// 			{
-// 				/* Error logged by open_walfile */
-// 				return false;
-// 			}
-// 		}
-//
-// 		if (stream->walmethod->ops->write(walfile,
-// 										  copybuf + hdr_len + bytes_written,
-// 										  bytes_to_write) != bytes_to_write)
-// 		{
-// 			pg_log_error("could not write %d bytes to WAL file \"%s\": %s",
-// 						 bytes_to_write, walfile->pathname,
-// 						 GetLastWalMethodError(stream->walmethod));
-// 			return false;
-// 		}
-//
-// 		/* Write was successful, advance our position */
-// 		bytes_written += bytes_to_write;
-// 		bytes_left -= bytes_to_write;
-// 		*blockpos += bytes_to_write;
-// 		xlogoff += bytes_to_write;
-//
-// 		/* Did we reach the end of a WAL segment? */
-// 		if (XLogSegmentOffset(*blockpos, WalSegSz) == 0)
-// 		{
-// 			if (!close_walfile(stream, *blockpos))
-// 				/* Error message written in close_walfile() */
-// 				return false;
-//
-// 			xlogoff = 0;
-//
-// 			if (still_sending && stream->stream_stop(*blockpos, stream->timeline, true))
-// 			{
-// 				if (PQputCopyEnd(conn, NULL) <= 0 || PQflush(conn))
-// 				{
-// 					pg_log_error("could not send copy-end packet: %s",
-// 								 PQerrorMessage(conn));
-// 					return false;
-// 				}
-// 				still_sending = false;
-// 				return true;	/* ignore the rest of this XLogData packet */
-// 			}
-// 		}
-// 	}
-// 	/* No more data left to write, receive next copy packet */
-//
-// 	return true;
-// }
 
 func ProcessXLogDataMsg(
 	conn *pgconn.PgConn,
@@ -516,108 +231,159 @@ func ProcessXLogDataMsg(
 	return true, nil
 }
 
-func HandleCopyStream(ctx context.Context, conn *pgconn.PgConn, stream *StreamCtl) (pglogrepl.LSN, error) {
-	lastStatusTime := time.Time{}
+// handle copy //
+
+func checkCopyStreamStop(
+	ctx context.Context,
+	conn *pgconn.PgConn,
+	stream *StreamCtl,
+	blockpos pglogrepl.LSN,
+) bool {
+	if stillSending && stream.StreamStop(blockpos, stream.Timeline, false) {
+		// Close WAL file first
+		if err := closeWalfile(stream, uint64(blockpos)); err != nil {
+			// Error already logged in closeWalFile
+			return false
+		}
+
+		// Send CopyDone
+		_, err := pglogrepl.SendStandbyCopyDone(ctx, conn)
+		if err != nil {
+			log.Printf("could not send copy-end packet: %v", err)
+			return false
+		}
+
+		stillSending = false
+	}
+
+	return true
+}
+
+func sendFeedback(
+	ctx context.Context,
+	conn *pgconn.PgConn,
+	blockpos pglogrepl.LSN,
+	now time.Time,
+	replyRequested bool,
+) error {
+	var standbyStatus pglogrepl.StandbyStatusUpdate
+
+	standbyStatus.WALWritePosition = blockpos
+
+	if reportFlushPosition {
+		standbyStatus.WALFlushPosition = lastFlushPosition
+	} else {
+		standbyStatus.WALFlushPosition = pglogrepl.LSN(0)
+	}
+
+	standbyStatus.WALApplyPosition = pglogrepl.LSN(0)
+	standbyStatus.ClientTime = now
+	standbyStatus.ReplyRequested = replyRequested
+
+	err := pglogrepl.SendStandbyStatusUpdate(ctx, conn, standbyStatus)
+	if err != nil {
+		log.Printf("could not send feedback packet: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func HandleCopyStream(ctx context.Context, conn *pgconn.PgConn, stream *StreamCtl) error {
+	var lastStatus time.Time
 	blockPos := stream.StartPos
 
 	stillSending = true
 
 	for {
-		// Check if we should stop
-		if stillSending && stream.StreamStop(blockPos, stream.Timeline, false) {
-			// gracefully end
-			return blockPos, nil
+		// Check if we should stop streaming
+		if !checkCopyStreamStop(ctx, conn, stream, blockPos) {
+			return errors.New("stream stop requested")
 		}
 
 		now := time.Now()
 
-		// If synchronous, flush + feedback immediately
-		if stream.Synchronous {
-			if err := stream.FlushWAL(); err != nil {
-				return 0, fmt.Errorf("flush WAL failed: %w", err)
+		// If synchronous, flush WAL file and update server immediately
+		if stream.Synchronous && lastFlushPosition < blockPos && walfile != nil {
+			if err := walfile.Sync(); err != nil {
+				return fmt.Errorf("could not fsync WAL file: %w", err)
 			}
-			if err := stream.SendFeedback(conn, blockPos, now, false); err != nil {
-				return 0, fmt.Errorf("send feedback failed: %w", err)
+			lastFlushPosition = blockPos
+			if err := sendFeedback(ctx, conn, blockPos, now, false); err != nil {
+				return fmt.Errorf("could not send feedback after sync: %w", err)
 			}
-			lastStatusTime = now
+			lastStatus = now
 		}
 
-		// If timeout elapsed, send a standby status update
-		if stream.StandbyMessageTimeout > 0 &&
-			now.After(lastStatusTime.Add(stream.StandbyMessageTimeout)) {
-			if err := stream.SendFeedback(conn, blockPos, now, false); err != nil {
-				return 0, fmt.Errorf("send feedback timed out: %w", err)
+		// If time to send feedback
+		if stillSending && stream.StandbyMessageTimeout > 0 &&
+			time.Since(lastStatus) > stream.StandbyMessageTimeout {
+			if err := sendFeedback(ctx, conn, blockPos, now, false); err != nil {
+				return fmt.Errorf("could not send periodic feedback: %w", err)
 			}
-			lastStatusTime = now
+			lastStatus = now
 		}
 
-		// Calculate how long we can block
-		sleepTime := calculateSleepTimeout(now, lastStatusTime, stream.StandbyMessageTimeout)
+		// Set next wake-up
+		timeout := stream.StandbyMessageTimeout
+		if timeout <= 0 {
+			timeout = 10 * time.Second
+		}
 
-		// Receive WAL from the server
-		ctxWithTimeout, cancel := context.WithTimeout(ctx, sleepTime)
-		msg, err := conn.ReceiveMessage(ctxWithTimeout)
+		ctxTimeout, cancel := context.WithTimeout(ctx, timeout)
+		msg, err := conn.ReceiveMessage(ctxTimeout)
 		cancel()
-
+		if pgconn.Timeout(err) {
+			continue
+		}
 		if err != nil {
-			if pgconn.Timeout(err) {
-				continue // normal, timed out waiting
-			}
-			return 0, fmt.Errorf("receive failed: %w", err)
+			return fmt.Errorf("receive message failed: %w", err)
 		}
 
 		switch m := msg.(type) {
+
 		case *pgproto3.CopyData:
 			switch m.Data[0] {
 			case pglogrepl.PrimaryKeepaliveMessageByteID:
-				keepalive, err := pglogrepl.ParsePrimaryKeepaliveMessage(m.Data[1:])
+				pkm, err := pglogrepl.ParsePrimaryKeepaliveMessage(m.Data[1:])
 				if err != nil {
-					return 0, fmt.Errorf("parse keepalive failed: %w", err)
+					return fmt.Errorf("parse keepalive failed: %w", err)
 				}
-				if keepalive.ReplyRequested {
-					if err := stream.SendFeedback(conn, blockPos, now, false); err != nil {
-						return 0, fmt.Errorf("keepalive feedback failed: %w", err)
+				if pkm.ReplyRequested {
+					if err := sendFeedback(ctx, conn, blockPos, time.Now(), false); err != nil {
+						return fmt.Errorf("send feedback on reply requested failed: %w", err)
 					}
-					lastStatusTime = now
+					lastStatus = time.Now()
 				}
 
 			case pglogrepl.XLogDataByteID:
 				xld, err := pglogrepl.ParseXLogData(m.Data[1:])
 				if err != nil {
-					return 0, fmt.Errorf("parse XLogData failed: %w", err)
+					return fmt.Errorf("parse xlogdata failed: %w", err)
 				}
-				if err := stream.WriteXLogData(&xld); err != nil {
-					return 0, fmt.Errorf("writing xlogdata failed: %w", err)
-				}
-				blockPos = xld.WALStart + pglogrepl.LSN(len(xld.WALData))
 
-				// Recheck if we should stop after writing
-				if stillSending && stream.StreamStop(blockPos, stream.Timeline, true) {
-					return blockPos, nil
+				// TODO:types:fix
+				bp := uint64(blockPos)
+				// TODO:design:fix, now this will fail
+				if _, err := ProcessXLogDataMsg(conn, stream, xld.WALData, &bp); err != nil {
+					return fmt.Errorf("processing xlogdata failed: %w", err)
+				}
+
+				// After processing XLogData, check again if stop is requested
+				if !checkCopyStreamStop(ctx, conn, stream, blockPos) {
+					return errors.New("stream stop requested after XLogData")
 				}
 
 			default:
-				return 0, fmt.Errorf("unexpected CopyData message type: %v", m.Data[0])
+				return fmt.Errorf("unexpected CopyData message type: %c", m.Data[0])
 			}
 
 		case *pgproto3.CopyDone:
-			// Server indicates end of stream
-			return blockPos, nil
+			// Server ended streaming normally
+			return nil
 
 		default:
-			return 0, fmt.Errorf("unexpected server message %T", msg)
+			return fmt.Errorf("unexpected backend message: %T", msg)
 		}
 	}
-}
-
-// Helper: How long to wait for next standby_message_timeout
-func calculateSleepTimeout(now, lastStatus time.Time, standbyMessageTimeout time.Duration) time.Duration {
-	if standbyMessageTimeout <= 0 {
-		return time.Second * 10 // default
-	}
-	next := lastStatus.Add(standbyMessageTimeout)
-	if next.After(now) {
-		return next.Sub(now)
-	}
-	return time.Second
 }
