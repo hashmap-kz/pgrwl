92198fc23bc068c2fad245df467e35322e018a9d
alexey.zh <mailbox.stdio@gmail.com>
2025-04-26 15:11:55 +0500

    fix

--- internal/xlog/pg_receivewal.go
+++ internal/xlog/pg_receivewal.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -12,7 +13,12 @@ import (
 	"github.com/jackc/pglogrepl"
 )
 
-var walFileRe = regexp.MustCompile(`^([0-9A-F]{8})([0-9A-F]{8})([0-9A-F]{8})(\.partial)?$`)
+var (
+	walFileRe                = regexp.MustCompile(`^([0-9A-F]{8})([0-9A-F]{8})([0-9A-F]{8})(\.partial)?$`)
+	verbose                  = true
+	timeToStop               = false
+	endpos     pglogrepl.LSN = 0
+)
 
 // FindStreamingStart scans baseDir for WAL files and returns (startLSN, timeline)
 func FindStreamingStart(baseDir string) (pglogrepl.LSN, uint32, error) {
@@ -104,4 +110,98 @@ func parseHex32(s string) (uint32, error) {
 
 func segNoToLSN(segNo uint64) pglogrepl.LSN {
 	return pglogrepl.LSN(segNo * WalSegSz)
+}
+
+// stop
+
+// static bool
+// stop_streaming(XLogRecPtr xlogpos, uint32 timeline, bool segment_finished)
+// {
+// 	static uint32 prevtimeline = 0;
+// 	static XLogRecPtr prevpos = InvalidXLogRecPtr;
+//
+// 	/* we assume that we get called once at the end of each segment */
+// 	if (verbose && segment_finished)
+// 		pg_log_info("finished segment at %X/%X (timeline %u)",
+// 					LSN_FORMAT_ARGS(xlogpos),
+// 					timeline);
+//
+// 	if (!XLogRecPtrIsInvalid(endpos) && endpos < xlogpos)
+// 	{
+// 		if (verbose)
+// 			pg_log_info("stopped log streaming at %X/%X (timeline %u)",
+// 						LSN_FORMAT_ARGS(xlogpos),
+// 						timeline);
+// 		time_to_stop = true;
+// 		return true;
+// 	}
+//
+// 	/*
+// 	 * Note that we report the previous, not current, position here. After a
+// 	 * timeline switch, xlogpos points to the beginning of the segment because
+// 	 * that's where we always begin streaming. Reporting the end of previous
+// 	 * timeline isn't totally accurate, because the next timeline can begin
+// 	 * slightly before the end of the WAL that we received on the previous
+// 	 * timeline, but it's close enough for reporting purposes.
+// 	 */
+// 	if (verbose && prevtimeline != 0 && prevtimeline != timeline)
+// 		pg_log_info("switched to timeline %u at %X/%X",
+// 					timeline,
+// 					LSN_FORMAT_ARGS(prevpos));
+//
+// 	prevtimeline = timeline;
+// 	prevpos = xlogpos;
+//
+// 	if (time_to_stop)
+// 	{
+// 		if (verbose)
+// 			pg_log_info("received interrupt signal, exiting");
+// 		return true;
+// 	}
+// 	return false;
+// }
+
+func stopStreaming(xlogpos pglogrepl.LSN, timeline uint32, segmentFinished bool) bool {
+	var (
+		prevTimeline uint32
+		prevPos      pglogrepl.LSN
+	)
+
+	if verbose && segmentFinished {
+		log.Printf(
+			"finished segment at %X/%X (timeline %d)",
+			uint32(xlogpos>>32), uint32(xlogpos), timeline,
+		)
+	}
+
+	if endpos != 0 && endpos < xlogpos {
+		if verbose {
+			log.Printf(
+				"stopped log streaming at %X/%X (timeline %d)",
+				uint32(xlogpos>>32), uint32(xlogpos), timeline,
+			)
+		}
+		timeToStop = true
+		return true
+	}
+
+	if verbose && prevTimeline != 0 && prevTimeline != timeline {
+		log.Printf(
+			"switched to timeline %d at %X/%X",
+			timeline,
+			uint32(prevPos>>32), uint32(prevPos),
+		)
+	}
+
+	prevTimeline = timeline
+	prevPos = xlogpos
+
+	if timeToStop {
+		if verbose {
+			log.Printf("received interrupt signal, exiting")
+		}
+		return true
+	}
+
+	return false
 }
