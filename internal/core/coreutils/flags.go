00d168f21e7fd2cbce8de9d2b1e5eb580da9c6c5
alexey.zh <mailbox.stdio@gmail.com>
2025-05-08 09:26:30 +0500

    stats - Update usage message.

--- internal/core/coreutils/flags.go
+++ internal/core/coreutils/flags.go
@@ -39,21 +39,60 @@ func ParseFlags() (*Opts, error) {
 	flag.StringVar(&opts.HTTPServerAddr, "http-server-addr", "", "")
 	flag.StringVar(&opts.HTTPServerToken, "http-server-token", "", "")
 	flag.Usage = func() {
-		_, _ = fmt.Fprintf(os.Stderr, `Usage: pgrwl [OPTIONS]
+		_, _ = fmt.Fprintf(os.Stderr, `Run WAL-receiver
+
+Examples:
+  # Set PostgreSQL connection parameters
+  export PGHOST='localhost'
+  export PGPORT='5432'
+  export PGUSER='postgres'
+  export PGPASSWORD='postgres'
+
+  # Start receiving WAL files into local directory '/mnt/wal-archive'
+  # using the replication slot named 'bookstore_app'
+  pgrwl -D /mnt/wal-archive -S bookstore_app
 
 Main Options:
-  -D, --directory       receive write-ahead log files into this directory (required)
-  -S, --slot            replication slot to use (required)
-  -n, --no-loop         do not loop on connection lost
-      --log-level       set log level (trace, debug, info, warn, error) (default: info)
-      --log-format      specify log formatter (json, text) (default: json)
-      --log-add-source  include source file and line in log output (default: false)
+  -D, --directory='':
+      Target directory to store WAL files. Required.
+      ENV: PGRWL_DIRECTORY
 
-Optional features:
-  --http-server-addr    If set, runs an HTTP server in the background (for management, stats, etc.)
-                        Example: "--http-server-addr=:8080"
-  --http-server-token   Required when the HTTP server is enabled (treat this as a password)
-                        Example: "--http-server-token=${MY_SECRET_PASS}"
+  -S, --slot='':
+      Replication slot to use. Required.
+      ENV: PGRWL_SLOT
+
+  -n, --no-loop=false:
+      If set, do not loop on connection loss.
+      ENV: PGRWL_NO_LOOP
+
+Logging:
+  --log-level=info:
+      Set log verbosity: trace, debug, info, warn, error.
+      ENV: PGRWL_LOG_LEVEL
+
+  --log-format=json:
+      Specify log formatter: json or text.
+      ENV: PGRWL_LOG_FORMAT
+
+  --log-add-source=false:
+      Include file and line info in logs if set.
+      ENV: PGRWL_LOG_ADD_SOURCE
+
+Optional HTTP Server:
+  --http-server-addr='':
+      Run a background HTTP server for monitoring and control.
+      ENV: PGRWL_HTTP_SERVER_ADDR
+
+  --http-server-token='':
+      Token required for accessing the HTTP server.
+      ENV: PGRWL_HTTP_SERVER_TOKEN
+
+Help:
+  -h, --help:
+      Show this help message and exit.
+
+Usage:
+  pgrwl -D DIRECTORY -S SLOT [options]
 `)
 	}
 	flag.Parse()
