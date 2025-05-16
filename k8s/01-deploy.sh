a86d189b31ef8691ab5caca6c1fa6e04a52c2ab3
alexey.zh <mailbox.stdio@gmail.com>
2025-05-16 09:00:54 +0500

    Update k8s deploys

--- k8s/01-deploy.sh
+++ k8s/01-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+(
+  cd .. && make image
+)
+
 kubectl apply -f manifests/
 kubectl -n pgrwl-test rollout restart sts pgrwl
 kubectl -n pgrwl-test rollout restart sts postgres
