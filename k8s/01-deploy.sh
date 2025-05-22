5811336761d8b96453974fe825c451ca37e42238
alexey.zh <mailbox.stdio@gmail.com>
2025-05-22 08:24:23 +0500

    Manifests, minio (for tests and examples)

--- k8s/01-deploy.sh
+++ k8s/01-deploy.sh
@@ -5,12 +5,14 @@ set -euo pipefail
   cd .. && make image
 )
 
-kubectl create ns pgrwl-test --dry-run -oyaml | kubectl apply -f -
+kubectl create ns pgrwl-test --dry-run=client -oyaml | kubectl apply -f -
 
 kubectl -n pgrwl-test create secret generic minio-certs \
   --from-file=public.crt=./files/minio/certs/public.crt \
-  --from-file=private.key=./files/minio/certs/private.key
+  --from-file=private.key=./files/minio/certs/private.key \
+  --dry-run=client -oyaml | kubectl apply -f -
 
 kubectl apply -f manifests/
-kubectl -n pgrwl-test rollout restart sts pgrwl
 kubectl -n pgrwl-test rollout restart sts postgres
+kubectl -n pgrwl-test rollout restart sts minio
+kubectl -n pgrwl-test rollout restart sts pgrwl
