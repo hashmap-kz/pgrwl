ed0ae1d409aae0caf22af52cd9934f297b4aa0a9
alexey.zh <mailbox.stdio@gmail.com>
2025-05-27 11:00:24 +0500

    metricsv2 - Update monitoring infra

--- k8s/01-deploy.sh
+++ k8s/01-deploy.sh
@@ -7,11 +7,19 @@ set -euo pipefail
 
 kubectl create ns pgrwl-test --dry-run=client -oyaml | kubectl apply -f -
 
+# create grafana dashboards
+kubectl create configmap grafana-dashboards \
+  --from-file=dashboards/ \
+  --namespace=pgrwl-test \
+  --dry-run=client -oyaml | kubectl apply -f -
+
+# create minio certs
 kubectl -n pgrwl-test create secret generic minio-certs \
   --from-file=public.crt=./files/minio/certs/public.crt \
   --from-file=private.key=./files/minio/certs/private.key \
   --dry-run=client -oyaml | kubectl apply -f -
 
+# deploy and restart workloads
 kubectl apply -f manifests/
 kubectl -n pgrwl-test rollout restart sts postgres
 kubectl -n pgrwl-test rollout restart sts minio
