82229f1768cd90ee50671c3ec6ab0a3d1bd38637
alexey.zh <mailbox.stdio@gmail.com>
2026-03-27 20:49:23 +0500

    fix: working on https://github.com/hashmap-kz/pgrwl/issues/186

--- internal/opt/modes/backupmode/streaming_file.go
+++ internal/opt/modes/backupmode/streaming_file.go
@@ -5,20 +5,26 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"sync"
 
 	st "github.com/hashmap-kz/storecrypt/pkg/storage"
 )
 
 type StreamingFile struct {
-	path   string
-	pw     *io.PipeWriter
-	done   chan struct{}
+	path string
+	pw   *io.PipeWriter
+
+	done chan struct{}
+	log  *slog.Logger
+
+	mu     sync.Mutex
 	putErr error
-	log    *slog.Logger
+	closed bool
 }
 
 func NewStreamingFile(ctx context.Context, log *slog.Logger, storage st.Storage, path string) *StreamingFile {
 	pr, pw := io.Pipe()
+
 	sf := &StreamingFile{
 		path: path,
 		pw:   pw,
@@ -28,16 +34,34 @@ func NewStreamingFile(ctx context.Context, log *slog.Logger, storage st.Storage,
 
 	go func() {
 		defer func() {
+			_ = pr.Close()
 			log.Info("closing", slog.String("file", path))
 			close(sf.done)
 		}()
-		sf.putErr = storage.Put(ctx, path, pr)
+
+		err := storage.Put(ctx, path, pr)
+
+		sf.mu.Lock()
+		sf.putErr = err
+		sf.mu.Unlock()
+
+		if err != nil {
+			_ = pw.CloseWithError(err)
+		}
 	}()
 
 	return sf
 }
 
 func (sf *StreamingFile) Write(p []byte) (int, error) {
+	sf.mu.Lock()
+	closed := sf.closed
+	sf.mu.Unlock()
+
+	if closed {
+		return 0, fmt.Errorf("write to closed streaming file: %s", sf.path)
+	}
+
 	return sf.pw.Write(p)
 }
 
@@ -45,8 +69,24 @@ func (sf *StreamingFile) Close() error {
 	if sf == nil {
 		return nil
 	}
-	_ = sf.pw.Close()
+
+	sf.mu.Lock()
+	if sf.closed {
+		sf.mu.Unlock()
+		return nil
+	}
+	sf.closed = true
+	sf.mu.Unlock()
+
+	if err := sf.pw.Close(); err != nil {
+		return err
+	}
+
 	<-sf.done
+
+	sf.mu.Lock()
+	defer sf.mu.Unlock()
+
 	if sf.putErr != nil {
 		return fmt.Errorf("storage put failed for %s: %w", sf.path, sf.putErr)
 	}
