ed02d27e36c080003879cc722a956532a46a6384
alexey.zh <mailbox.stdio@gmail.com>
2026-04-18 13:08:59 +0500

    refactor: update localdev integration tests

--- test/integration/k8s/01-deploy.sh
+++ test/integration/k8s/01-deploy.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-LOCALDEV=true
-IMAGE="quay.io/pgrwl/pgrwl:latest"
-
-if [[ "$LOCALDEV" == "true" ]]; then
-  (
-    cd ../../../ && make image
-  )
-  IMAGE='localhost:5000/pgrwl:latest'
-fi
-
-sed -i "s|__PGRWL_IMAGE__|${IMAGE}|g" manifests/04-pgrwl-receive.yaml
-sed -i "s|__PGRWL_IMAGE__|${IMAGE}|g" manifests/05-pgrwl-backup.yaml
+(
+  cd ../../../ && make image
+)
 
 kubectl create ns pgrwl-test --dry-run=client -oyaml | kubectl apply -f -
 kubectl create ns mon --dry-run=client -oyaml | kubectl apply -f -
