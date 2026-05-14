84cb43b7b7e8de7b77a4ad78cf40cbe5c2b04ab0
alexey.zh <mailbox.stdio@gmail.com>
2026-05-14 09:04:17 +0500

    feat(integ): fake-reader for simulate streaming multipart upload

---
+++ test/integration/storage/multipart_rdr_test.go
@@ -0,0 +1,36 @@
+package integration
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/pgrwl/pgrwl/internal/opt/shared/readers"
+	storage "github.com/pgrwl/pgrwl/internal/opt/shared/storecrypt"
+	"github.com/stretchr/testify/require"
+)
+
+func TestS3Storage_PutMultipart50Gi(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Minute)
+	defer cancel()
+
+	client := createS3Client()
+	prefix := t.Name()
+	st := storage.NewS3Storage(client, "backups", prefix)
+
+	r := readers.NewFakeLargeReader(readers.S50Gi)
+	// wrap to hide size (IMPORTANT)
+	reader := io.NopCloser(r)
+
+	err := st.Put(ctx, "stream.bin", reader)
+	require.NoError(t, err)
+
+	info, err := st.ListInfo(ctx, "")
+	require.NoError(t, err)
+
+	require.Equal(t, 1, len(info))
+	require.Equal(t, readers.S50Gi, info[0].Size)
+}
